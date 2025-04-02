7dc71cee382769bb8cb4ac1ff08ea9339fcce93a
preschmf <44412371+preschmf@users.noreply.github.com>
2025-04-01 22:26:07 -0400

    dynamic owner

--- adapters/gitHubRequests.js
+++ adapters/gitHubRequests.js
@@ -2,6 +2,7 @@ import { Octokit } from '@octokit/rest'
 
 const gitHubAuthToken = process.env.GITHUB_AUTH_TOKEN
 const gitHubEnterpriseBaseUrl = process.env.GITHUB_ENTERPRISE_BASE_URL
+const gitHubOwner = process.env.GITHUB_OWNER
 
 const octokit = new Octokit({
     auth: gitHubAuthToken,
@@ -35,7 +36,7 @@ export const getUserName = async () => {
 
 export const getReviewsOnPR = async (pullNumber, repo) => {
     const { data } = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
-        owner: 'claim',
+        owner: gitHubOwner,
         repo: repo,
         pull_number: pullNumber,
         headers: {
